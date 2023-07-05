9833c1eb85f0d936a4a1d99e6fc11bd3b586008c
Aditya Pratama <aditypraa@gmail.com>
2023-07-05 09:13:28 +0700

    Materi Pertemuan 4 - Conditional

---
+++ 3_JavaScript-Niomic/4.Pertemuan 4 - Conditional/nested-if-else.js
@@ -0,0 +1,16 @@
+let x = 30;
+let y = 20;
+
+if (x > y) {
+  if (x > y) {
+    console.log("X adalah Yang paling Besar");
+  } else {
+    console.log("Nilai X adalah yang paling besar kedua");
+  }
+} else {
+  if (x > y) {
+    console.log("X adalah yang paling kecil");
+  } else {
+    console.log("Nilai X adalah yang terkecil kedua");
+  }
+}
