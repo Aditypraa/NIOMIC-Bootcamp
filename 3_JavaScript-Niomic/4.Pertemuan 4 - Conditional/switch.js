c1ef7a11c2e0bbbe622eafcf64b1fea88c45bfa8
Aditya Pratama <aditypraa@gmail.com>
2023-07-05 14:31:31 +0700

    Materi Pertemuan 4 - Conditional (If Else & Switch)

---
+++ 3_JavaScript-Niomic/4.Pertemuan 4 - Conditional/switch.js
@@ -0,0 +1,16 @@
+function belajarSwitch() {
+  let nilai = 20;
+
+  switch (nilai) {
+    case 10:
+      console.log("Nilai Mata Kuliah kurang baik");
+      break;
+    case 20:
+      console.log("Nilai Mata kuliah anda baik");
+      break;
+    default:
+      console.log("Nilai mata kuliah anda Melampaui batas");
+  }
+}
+
+belajarSwitch();
