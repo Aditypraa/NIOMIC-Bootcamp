38f5df3b2b239d23721c0baf8c809de9330113d0
Aditya Pratama <aditypraa@gmail.com>
2023-07-19 23:41:46 +0700

    12.Pertemuan 12 - Operasi

---
+++ 3_JavaScript-Niomic/12.Pertemuan 12 - Operasi/operasi.js
@@ -0,0 +1,19 @@
+let motor = ["Supra", "Satria", "Vario"];
+// * Pop akan menghapus Index dari belakang
+motor.pop();
+console.log(`Pop Menghapus Index dari belakang : ${motor}`);
+
+let mobil = ["avanza", "toyota"];
+// * Push akan menambah index dari belakang
+mobil.push("Daihatsu");
+console.log(`Push akan menambah index dari belakang : ${mobil}`);
+
+let buah = ["Apel", "mangga", "jeruk"];
+// * Shift akan menghapus index dari depan
+buah.shift();
+console.log(`Shift akan menghapus index dari Depan : ${buah}`);
+
+let people = ["yadi", "yanto"];
+// * Unshift akan menambah index dari DEPAN
+people.unshift("Annisa");
+console.log(`Unshift akan menambah index dari depan : ${people}`);
