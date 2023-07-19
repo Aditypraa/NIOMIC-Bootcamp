bba4c56a5a924c475f7795f7a367a3753d9555bc
Aditya Pratama <aditypraa@gmail.com>
2023-07-19 21:43:26 +0700

    Continue dan Break pada for

--- 3_JavaScript-Niomic/5.Pertemuan 5 - Looping/for.js
+++ 3_JavaScript-Niomic/5.Pertemuan 5 - Looping/for.js
@@ -2,6 +2,10 @@ function perulanganFor() {
   //   let i = 0;
 
   for (let i = 0; i < 10; i++) {
+    if (i == 5) {
+      //break; // Maka Hasilnya akan berhenti jika kondisi mencapai 5
+      continue; // Maka Nomor 5 Akan Dilewati
+    }
     console.log(`Jalan : ${i}`);
   }
 }
