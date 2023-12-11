ab22c8169c1f5b9e0bfcd77b939b257ad5af6a97
Aditya Pratama <aditypraa@gmail.com>
2023-12-12 00:40:28 +0700

    Dea Afrizal : Javascript

--- JavaScript-PZN/Array/array.js
+++ JavaScript-PZN/Array/array.js
@@ -2,15 +2,11 @@ let arrayKosong = [];
 
 arrayKosong.push("Tambah1"); // Menambah Data Array
 arrayKosong.push("Tambah2", "Tambah3"); //Menambah Data Array Lebih Dari Satu
-
 console.log(arrayKosong.length); // Untuk Mendapatkan Panjang Array
+console.log(arrayKosong[1]); // Untuk Mendapatkan Array Dari Index ke 1
 
-console.log(arrayKosong[1]); // Untuk Mendapatkan Array Dari Idex ke 1
-
-arrayKosong[0] = "Ini saya Rubah"; // Merubah Index ke 1
-
+arrayKosong[0] = "Ini saya Rubah"; // Merubah Index ke 0
 delete arrayKosong[1]; // Menghapus Data Array Index ke 1 tetapi tidak dengan tempatnya
-
 console.log(arrayKosong);
 
 // Penggunaan Array Dengan Index
