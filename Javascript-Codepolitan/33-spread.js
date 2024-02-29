c03e6ce7fe6c2a5ef249f5641613c020015b33af
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-03-01 01:12:30 +0700

    (Javascript-Codepolitan) : 32-Spread

---
+++ Javascript-Codepolitan/33-spread.js
@@ -0,0 +1,4 @@
+// Spread : Mengubah suatu arry atau object atau string menjadi deret parameter untuk function
+const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+const coba1 = Math.max(...angka);
+console.log(coba1);
