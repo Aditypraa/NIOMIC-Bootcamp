0e37e5812e582ad8a264a30781d7fc2c5b5e833f
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-03-28 00:23:34 +0700

    (Javascript Codepolitan - EJS) : Javascript-Codepolitan EJS

---
+++ Express-Codepolitan/2-ejs/index.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/random", (req, res) => {
+  const random = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { random });
+});
+
+app.listen(8080, () => {
+  console.log(`Listening on http://localhost:8080`);
+});
