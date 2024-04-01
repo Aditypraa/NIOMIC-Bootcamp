1ba0347f864a4e0151f2e2658940c45a7609e5a6
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-04-02 02:12:03 +0700

    (ReactJs-WPU) : 1-3

---
+++ ReactJs-WPU/2-installasi-dan-konfigurasi/src/components/Header.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line react/prop-types
+function Header({ author }) {
+  return <h1>Belajar React Js bang bareng {author ? author : "Aditya"}</h1>;
+}
+
+export default Header;
