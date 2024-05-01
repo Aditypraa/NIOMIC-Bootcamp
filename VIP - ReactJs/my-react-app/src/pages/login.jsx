bb01c78e9f1de076d1c47b3cdd4fc32524dcebd4
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-05-01 18:26:38 +0700

    (React Router) : React Router DOM

--- VIP - ReactJs/my-react-app/src/pages/login.jsx
+++ VIP - ReactJs/my-react-app/src/pages/login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import FormLogin from "../components/Fragments/FormLogin";
 import AuthLayouts from "../components/Layouts/AuthLayouts";
 
@@ -5,6 +6,17 @@ const LoginPage = () => {
   return (
     <AuthLayouts title="Login">
       <FormLogin />
+      <p className="text-sm mt-5 text-center">
+        Don`t have an account?{" "}
+        {/* ======== Jangan Menggunakan Tag a ======== */}
+        {/* <a href="/register" className="font-bold text-blue-600">
+          Sign Up
+        </a> */}
+        {/* ======== Gunakan <Link to/> untuk navigasi ======== */}
+        <Link to="/register" className="font-bold text-blue-600">
+          Sign Up
+        </Link>
+      </p>
     </AuthLayouts>
   );
 };
--- VIP - ReactJs/my-react-app/src/pages/register.jsx
+++ VIP - ReactJs/my-react-app/src/pages/register.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import FormRegister from "../components/Fragments/FormRegister";
 import AuthLayouts from "../components/Layouts/AuthLayouts";
 
@@ -5,6 +6,16 @@ const RegisterPage = () => {
   return (
     <AuthLayouts title="Register">
       <FormRegister />
+      <p className="text-sm mt-5 text-center">
+        have an account? {/* ======== Jangan Menggunakan Tag a ======== */}
+        {/* <a href="/register" className="font-bold text-blue-600">
+          Sign Up
+        </a> */}
+        {/* ======== Gunakan <Link to/> untuk navigasi ======== */}
+        <Link to="/login" className="font-bold text-blue-600">
+          Login
+        </Link>
+      </p>
     </AuthLayouts>
   );
 };
