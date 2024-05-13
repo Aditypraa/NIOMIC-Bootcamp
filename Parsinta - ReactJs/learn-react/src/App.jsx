5dab6aed651d76ff0d9459a62f911e12c93d9ba2
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-05-13 22:44:07 +0700

    Setup Project

---
+++ Parsinta - ReactJs/learn-react/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="bg-blue-600 grid place-content-center min-h-screen">
+        Learn React
+      </h1>
+    </div>
+  );
+}
